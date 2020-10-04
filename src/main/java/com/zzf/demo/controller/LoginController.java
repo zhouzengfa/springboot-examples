3da601bbc2bcd8748d0df1e3f47fc3f3f56b07c5
zhouzengfa <zengfa_zhou@163.com>
2020-10-04 15:44:12 +0800

    【实验】用户登陆

---
+++ src/main/java/com/zzf/demo/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.zzf.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @PostMapping("/user/login")
+    public String login(@RequestParam("username") String username,
+    @RequestParam("password") String password, Map<String, Object> map)
+    {
+        if (!StringUtils.isEmpty(username) && password.equals("123456"))
+        {
+            return "dashboard";
+        }
+        else
+        {
+            map.put("error", "用户名密码错误");
+            return "login";
+        }
+    }
+}
