8e2b2a04e68f81659d21dc9ae6d436343d15440d
zhouzengfa <zengfa_zhou@163.com>
2020-10-04 20:05:03 +0800

    【实验】员工数据获取，并渲染

---
+++ src/main/java/com/zzf/demo/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.zzf.demo.controller;
+
+import com.zzf.demo.dao.EmployeeDao;
+import com.zzf.demo.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> all = employeeDao.getAll();
+        model.addAttribute("emps", all);
+        return "emp/list";
+    }
+}
