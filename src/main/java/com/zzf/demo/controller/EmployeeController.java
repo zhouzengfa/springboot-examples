96b05a96911c346f3fccda11cb3edf73598ace7a
zhouzengfa <zengfa_zhou@163.com>
2020-10-04 22:05:56 +0800

    【实验】员工添加页面渲染及跳转

--- src/main/java/com/zzf/demo/controller/EmployeeController.java
+++ src/main/java/com/zzf/demo/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.zzf.demo.controller;
 
+import com.zzf.demo.dao.DepartmentDao;
 import com.zzf.demo.dao.EmployeeDao;
+import com.zzf.demo.entities.Department;
 import com.zzf.demo.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,10 +17,20 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("emps", all);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "/emp/add";
     }
 }
