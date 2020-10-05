1504ab7c99efc1859b11278a21bc142b7d7efe99
zhouzengfa <zengfa_zhou@163.com>
2020-10-05 21:14:39 +0800

    【web开发】 注册Filter组件

--- src/main/java/com/zzf/demo/config/MyServerConfig.java
+++ src/main/java/com/zzf/demo/config/MyServerConfig.java
@@ -1,9 +1,13 @@
 package com.zzf.demo.config;
 
+import com.zzf.demo.filter.MyFilter;
 import com.zzf.demo.servlet.MyServlet;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
 
 @Controller
 public class MyServerConfig {
@@ -11,5 +15,14 @@ public class MyServerConfig {
     public ServletRegistrationBean servletRegistrationBean(){
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new MyServlet(), "/MyServlet");
         return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new MyFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/", "/MyServlet"));
+
+        return filterRegistrationBean;
     }
 }
