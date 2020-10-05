eb1f663f62036278e7a761a2e98ba4d65d0fae43
zhouzengfa <zengfa_zhou@163.com>
2020-10-05 17:05:52 +0800

    【实验】 实现员工数据的删除

--- src/main/java/com/zzf/demo/controller/EmployeeController.java
+++ src/main/java/com/zzf/demo/controller/EmployeeController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -67,6 +64,13 @@ public class EmployeeController {
     public String empModify(Employee employee){
         System.out.println(employee);
         employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    //删除员工
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }
