ec6f41409c9df8d039ca576d4b3aa0e872ea6cd6
zhouzengfa <zengfa_zhou@163.com>
2020-10-06 11:59:33 +0800

    【数据访问】druid配置数据源监控

--- src/main/java/com/zzf/demo/config/DruidConfig.java
+++ src/main/java/com/zzf/demo/config/DruidConfig.java
@@ -1,16 +1,54 @@
 package com.zzf.demo.config;
 
+import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class DruidConfig {
 
+    // 配置数据源
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DruidDataSource druidDataSource(){
         return new DruidDataSource();
+    }
+
+    //配置Druid监控
+    //1.配置一个管理后台的Servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, String> params = new HashMap<>();
+        params.put("loginUsername", "admin");
+        params.put("loginPassword", "admin");
+        params.put("allow", "");
+
+        bean.setInitParameters(params);
+        return bean;
+    }
+
+    //2.配置一个web监控的Filter
+    @Bean
+    public FilterRegistrationBean statFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> params = new HashMap<>();
+        params.put("exclusions", "*.js,*.css,/druid/*");
+
+        bean.setInitParameters(params);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+
+        return bean;
     }
 }
