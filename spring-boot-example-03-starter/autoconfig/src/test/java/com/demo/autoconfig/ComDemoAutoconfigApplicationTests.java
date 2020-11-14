781eaf7ef663e3038be40a8963c71f9c11b36508
zhouzengfa <275699969@qq.com>
2020-11-14 16:16:06 +0800

    【自创Starter】01 创建三个工程 autoconfig、test为Web工程 starter为Maven工程

---
+++ spring-boot-example-03-starter/autoconfig/src/test/java/com/demo/autoconfig/ComDemoAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.autoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComDemoAutoconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
